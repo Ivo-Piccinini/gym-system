83ee1ba280dfd13d05d1956ec7c931810b440fd5
AgustinCarricart <aguscarricart1998@gmail.com>
2026-04-28 20:25:42 -0300

    Creado RoutineXEjercice
    Cree la tabla intermedia de rutina por ejercicio para la entidad

---
+++ src/main/java/com/utnGymGroup/gym_system/feature/routinexejercice/RoutineXEjerciceEntity.java
@@ -0,0 +1,35 @@
+package com.utnGymGroup.gym_system.feature.routinexejercice;
+
+
+import com.utnGymGroup.gym_system.feature.exercise.ExerciseEntity;
+import com.utnGymGroup.gym_system.feature.routine.RoutineEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RoutineXEjerciceEntity
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+  @ManyToOne
+  @JoinColumn (name = "routine_id")
+    private RoutineEntity routine;
+
+  @ManyToOne
+    @JoinColumn(name = "Exercise_id")
+    private ExerciseEntity exercise;
+
+  private Integer series;
+  private Integer reps;
+  private Integer weight;
+  
+}
