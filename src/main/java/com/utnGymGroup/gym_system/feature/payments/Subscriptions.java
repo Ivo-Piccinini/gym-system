90812da539d290544f6db566251644c0ce2f53b7
Facundo_Barreto <facubarreto2006@gmail.com>
2026-04-29 23:07:18 -0300

    Create Subscriptions.java

---
+++ src/main/java/com/utnGymGroup/gym_system/feature/payments/Subscriptions.java
@@ -0,0 +1,22 @@
+package com.utnGymGroup.gym_system.feature.payments;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "Exercise")
+public class Subscriptions {
+
+    
+
+
+
+}
