8c6caef0eb5dcc9adfa5ab0fe82e737fe68990a7
ramiromartinoo <ramiromartino04@gmail.com>
2026-04-30 12:29:18 -0300

    finalizacion entidades modulo activities

--- src/main/java/com/utnGymGroup/gym_system/features/activity/ActivityEntity.java
+++ src/main/java/com/utnGymGroup/gym_system/features/activity/ActivityEntity.java
@@ -2,6 +2,7 @@ package com.utnGymGroup.gym_system.features.activity;
 
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @AllArgsConstructor
@@ -13,10 +14,15 @@ import lombok.*;
 @Entity
 @Table(name = "activities")
 public class ActivityEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-private Long id;
-private String name;
-private String description;
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(length = 255,nullable = false)
+    private String description;
 
 }
