7a40a4adc21aab9f0301bf5bc2b6847cfc344af8
Rjbarret95 <150553611+Rjbarret95@users.noreply.github.com>
2026-05-05 12:20:25 -0300

    cree la clase paymentsDto

---
+++ src/main/java/com/utnGymGroup/gym_system/features/payments/PaymentsDTO.java
@@ -0,0 +1,30 @@
+package com.utnGymGroup.gym_system.features.payments;
+
+import com.utnGymGroup.gym_system.common.interfaces.ICreate;
+import com.utnGymGroup.gym_system.common.interfaces.IUpdate;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class PaymentsDTO {
+
+    @NotNull(message = "El monto es obligatorio", groups = ICreate.class)
+    @Positive(message = "El monto debe ser un valor positivo", groups = {ICreate.class,IUpdate.class})
+    private Double amount;
+
+    @NotNull(message = "La fecha es obligatoria", groups = ICreate.class)
+    private LocalDate paymentDate;
+
+    @NotNull(message = "El método de pago es obligatorio", groups = ICreate.class)
+    private PaymentMethods method;
+
+    @NotNull(message = "El ID de la suscripción es obligatorio", groups = ICreate.class)
+    private Long subscriptionId;
+}
