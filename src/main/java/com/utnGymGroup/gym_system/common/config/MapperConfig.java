08f905cfb321efcbaf97ddd9a8852062aef415e4
Ivo Piccinini <ivopiccinini02@gmail.com>
2026-05-22 15:20:17 -0300

    exceptions
    creacion de GlobalExceptionHandler y ErrorResponse para el manejo global de excepciones y creacion de excepciones para el modulo user/auth

--- src/main/java/com/utnGymGroup/gym_system/common/config/MapperConfig.java
+++ src/main/java/com/utnGymGroup/gym_system/common/config/MapperConfig.java
@@ -1,4 +1,4 @@
-package com.utnGymGroup.gym_system.common;
+package com.utnGymGroup.gym_system.common.config;
 
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
