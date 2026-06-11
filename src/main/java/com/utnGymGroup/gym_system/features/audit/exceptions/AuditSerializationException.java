be1f81fa3fea1176959b155fb0adbc59b57d25f7
Ivo <ivopiccinini02@gmail.com>
2026-06-11 11:41:08 -0300

    Update AuditSerializationException.java

--- src/main/java/com/utnGymGroup/gym_system/features/audit/exceptions/AuditSerializationException.java
+++ src/main/java/com/utnGymGroup/gym_system/features/audit/exceptions/AuditSerializationException.java
@@ -1,7 +1,11 @@
 package com.utnGymGroup.gym_system.features.audit.exceptions;
 
 public class AuditSerializationException extends RuntimeException {
+    
     public AuditSerializationException(String message) {
         super(message);
     }
-}
+
+    public AuditSerializationException(String message, Throwable cause) {
+        super(message, cause);
+    }}
